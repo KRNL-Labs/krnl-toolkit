86cb45fa537a1acb8ac9f9075557736faeebd7db
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2025-01-06 12:28:44 +0300

    The oasis sapphire had updated their contracts hence updating the import statements

--- smart-contracts/foundry/src/KRNL.sol
+++ smart-contracts/foundry/src/KRNL.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
